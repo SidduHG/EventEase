fa4dda88effa1e0b29fb581793e5657e4ee3a18d
SidduHG <sidduabd41@gmail.com>
2025-08-02 02:06:46 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors()
+);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI,{
@@ -31,6 +33,7 @@ const adminAnalyticsRoutes=require('./routes/adminAnalyticsRoutes')
 const feedbackRoutes = require('./routes/feedbackRoutes');
 const homeRoutes=require('./routes/homeRoutes')
 const paymentRoutes = require('./routes/paymentRoutes');
+
 
 
 
